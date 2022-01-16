cdb320037122272c9430511dc4c247fa7894ba58
erlebach <>
2022-01-16 15:38:56 -0500

    Analyze lines with various header prefixes. New file.

---
+++ read_original_text_files/interleave.py
@@ -0,0 +1,7 @@
+""" 
+Combine the files 
+[ bcc_txt cc_txt date_txt from_txt original_txt search_files.x subject_txt to_txt when_txt]
+to form an ordered list of "potential" email headers"]
+
+Assume that the emails in each file are ordered chronologically (this assumption might be incorrect)
+"""
